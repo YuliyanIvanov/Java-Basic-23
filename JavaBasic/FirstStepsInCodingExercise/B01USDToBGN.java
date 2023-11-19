97df5e45fd38685b2d6c01f227d0a5f54932d480
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-19 13:15:07 +0200

    Update for the second lesson.

---
+++ JavaBasic/FirstStepsInCodingExercise/B01USDToBGN.java
@@ -0,0 +1,11 @@
+package FirstStepsInCodingExercise;
+
+import java.util.Scanner;
+
+public class B01USDToBGN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
