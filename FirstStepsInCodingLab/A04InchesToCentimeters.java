1ec6e6f8965d8ccd7c662c912ba1692c6fbc8cd9
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-19 12:27:32 +0200

    Java-Basic 2023

---
+++ FirstStepsInCodingLab/A04InchesToCentimeters.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class A04InchesToCentimeters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double inch = Double.parseDouble(scanner.nextLine());
+
+        double centimeters = inch * 2.54;
+
+        System.out.println(centimeters);
+    }
+}
---
+++ FirstStepsInCodingLab/A07ProjectsCreation.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class A07ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
