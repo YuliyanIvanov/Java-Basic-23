2e76c23eff1c4539b88d7d66b0993b7974363d23
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-23 07:49:43 +0200

    Update for the seventh lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedLab/G03AnimalType.java
@@ -0,0 +1,10 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class G03AnimalType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
