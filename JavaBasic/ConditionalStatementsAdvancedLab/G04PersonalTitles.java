20d022262219c1ae2f759beb06100d5c6f8e9eaf
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-24 06:23:58 +0200

    Update for the seventh lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedLab/G04PersonalTitles.java
@@ -0,0 +1,29 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class G04PersonalTitles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double age = Double.parseDouble(scanner.nextLine());
+        String gender = scanner.nextLine();
+
+        switch (gender) {
+            case "m":
+                if (age < 16) {
+                    System.out.println("Master");
+                } else {
+                    System.out.println("Mr.");
+                }
+                break;
+            case "f":
+                if (age < 16) {
+                    System.out.println("Miss");
+                } else {
+                    System.out.println("Ms.");
+                }
+                break;
+        }
+    }
+}
