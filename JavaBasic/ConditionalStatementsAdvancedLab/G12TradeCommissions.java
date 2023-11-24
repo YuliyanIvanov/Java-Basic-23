bc928a01ece6e7eff67887514282dbdf525acd8f
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-24 07:47:35 +0200

    Update for the seventh lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedLab/G12TradeCommissions.java
@@ -0,0 +1,9 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class G12TradeCommissions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
