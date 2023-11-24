8d06d491209a9b05f4cfdfbff64883d5d593889b
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-24 07:12:46 +0200

    Update for the seventh lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedLab/G09FruitOrVegetable.java
@@ -0,0 +1,30 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class G09FruitOrVegetable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String type = scanner.nextLine();
+
+        switch (type) {
+            case "banana":
+            case "apple":
+            case "kiwi":
+            case "cherry":
+            case "lemon":
+            case "grapes":
+                System.out.println("fruit");
+                break;
+            case "tomato":
+            case "cucumber":
+            case "pepper":
+            case "carrot":
+                System.out.println("vegetable");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+    }
+}
