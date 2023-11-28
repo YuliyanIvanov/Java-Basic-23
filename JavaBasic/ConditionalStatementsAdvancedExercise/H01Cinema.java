77b20a319f43623e01025f2b18befa14ec17699d
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-28 10:00:05 +0200

    Update for the eight lesson.

---
+++ JavaBasic/ConditionalStatementsAdvancedExercise/H01Cinema.java
@@ -0,0 +1,9 @@
+package ConditionalStatementsAdvancedExercise;
+
+import java.util.Scanner;
+
+public class H01Cinema {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
