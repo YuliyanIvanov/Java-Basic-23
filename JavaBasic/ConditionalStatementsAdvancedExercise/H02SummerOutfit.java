3a2a1bd75ba5a9dbfc147bb74d8f04d51f5e9bc0
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-28 10:31:50 +0200

    Update for the eight lesson.

--- JavaBasic/ConditionalStatementsAdvancedExercise/H02SummerOutfit.java
+++ JavaBasic/ConditionalStatementsAdvancedExercise/H02SummerOutfit.java
@@ -6,11 +6,11 @@ public class H02SummerOutfit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double temperature = Double.parseDouble(scanner.nextLine());
+        int temperature = Integer.parseInt(scanner.nextLine());
         String timeOfDay = scanner.nextLine();
 
-        String outfit;
-        String shoe;
+        String outfit = "";
+        String shoe = "";
 
         switch (timeOfDay) {
             case "Morning":
@@ -25,6 +25,31 @@ public class H02SummerOutfit {
                     shoe = "Sandals";
                 }
                 break;
+            case "Afternoon":
+                if (temperature <= 18) {
+                    outfit = "Shirt";
+                    shoe = "Moccasins";
+                } else if (temperature <= 24) {
+                    outfit = "T-Shirt";
+                    shoe = "Sandals";
+                } else if (temperature >= 25) {
+                    outfit = "Swim Suit";
+                    shoe = "Barefoot";
+                }
+                break;
+            case "Evening":
+                if (temperature <= 18) {
+                    outfit = "Shirt";
+                    shoe = "Moccasins";
+                } else if (temperature <= 24) {
+                    outfit = "Shirt";
+                    shoe = "Moccasins";
+                } else if (temperature >= 25) {
+                    outfit = "Shirt";
+                    shoe = "Moccasins";
+                }
+                break;
         }
+        System.out.printf("It's %s degrees, get your %s and %s.", temperature, outfit, shoe);
     }
 }
