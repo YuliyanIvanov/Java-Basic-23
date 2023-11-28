b47e1d45a0eb44ea142f242fcf5af3b988915c25
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-28 10:06:44 +0200

    Update for the eight lesson.

--- JavaBasic/ConditionalStatementsAdvancedExercise/H01Cinema.java
+++ JavaBasic/ConditionalStatementsAdvancedExercise/H01Cinema.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class H01Cinema {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String type = scanner.nextLine();
+        int rows = Integer.parseInt(scanner.nextLine());
+        int columns = Integer.parseInt(scanner.nextLine());
+
+        double income = 0;
+
+        switch (type) {
+            case "Premiere":
+                income = rows * columns * 12.00;
+                break;
+            case "Normal":
+                income = rows * columns * 7.50;
+                break;
+            case "Discount":
+                income = rows * columns * 5.00;
+                break;
+        }
+        System.out.printf("%.2f leva", income);
     }
 }
