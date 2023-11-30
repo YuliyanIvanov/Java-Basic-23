2c9e04e5c2e7c3f117ac0020237428e6d8f557ff
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-11-30 09:16:50 +0200

    Update for the eight lesson.

---
+++ JavaBasic/ConditionalStatementsAdvancedExercise/H07HotelRoom.java
@@ -0,0 +1,9 @@
+package ConditionalStatementsAdvancedExercise;
+
+import java.util.Scanner;
+
+public class H07HotelRoom {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
