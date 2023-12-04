479f5a983c0195f3797923875b980ca3f42f8b83
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-04 07:24:09 +0200

    Update for the eight lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedExercise/H09SkiTrip.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsAdvancedExercise;
+
+import java.util.Scanner;
+
+public class H09SkiTrip {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
