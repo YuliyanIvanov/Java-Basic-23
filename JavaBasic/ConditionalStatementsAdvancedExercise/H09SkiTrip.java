b197d303249ceb7d186f9d080a434924c2f15d00
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-04 07:46:34 +0200

    Update for the eight lecture.

--- JavaBasic/ConditionalStatementsAdvancedExercise/H09SkiTrip.java
+++ JavaBasic/ConditionalStatementsAdvancedExercise/H09SkiTrip.java
@@ -6,6 +6,49 @@ public class H09SkiTrip {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int days = Integer.parseInt(scanner.nextLine());
+        String room = scanner.nextLine();
+        String feedback = scanner.nextLine();
 
+        double sum = days - 1;
+
+        switch (room) {
+            case "room for one person":
+                sum *= 18;
+                break;
+            case "apartment":
+                sum *= 25;
+                if (days < 10) {
+                    sum = sum * 0.7;
+                } else if (days <= 15) {
+                    sum = sum * 0.65;
+                } else {
+                    sum = sum * 0.5;
+                }
+                break;
+            case "president apartment":
+                sum *= 35;
+                if (days < 10) {
+                    sum = sum * 0.9;
+                } else if (days <= 15) {
+                    sum = sum * 0.85;
+                } else {
+                    sum = sum * 0.8;
+                }
+                break;
+            default:
+                break;
+        }
+        switch (feedback) {
+            case "positive":
+                sum *= 1.25;
+                break;
+            case "negative":
+                sum *= 0.9;
+                break;
+            default:
+                break;
+        }
+        System.out.printf("%.2f", sum);
     }
 }
