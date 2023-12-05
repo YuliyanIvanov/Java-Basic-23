ae593f5783b0457a0c19cfad467a16d84545e43d
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-05 07:38:59 +0200

    Update for the ninth lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedMoreExercises/I04CarToGo.java
@@ -0,0 +1,9 @@
+package ConditionalStatementsAdvancedMoreExercises;
+
+import java.util.Scanner;
+
+public class I04CarToGo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
