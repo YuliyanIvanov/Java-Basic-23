1940e00f36b41568de2f6c4b4ff44181ba4a4f83
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-06 07:52:28 +0200

    Update for the ninth lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedMoreExercises/I08PointOnRectangleBorder.java
@@ -0,0 +1,9 @@
+package ConditionalStatementsAdvancedMoreExercises;
+
+import java.util.Scanner;
+
+public class I08PointOnRectangleBorder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
