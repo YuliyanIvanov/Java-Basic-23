97596394500f6c52b90cc7b448b790259538e4cf
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-06 06:50:03 +0200

    Update for the ninth lecture.

--- JavaBasic/ConditionalStatementsAdvancedMoreExercises/I05Vacation.java
+++ JavaBasic/ConditionalStatementsAdvancedMoreExercises/I05Vacation.java
@@ -6,6 +6,45 @@ public class I05Vacation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double budget = Double.parseDouble(scanner.nextLine());
+        String season = scanner.nextLine();
 
+        double price = 0;
+        String location = "";
+        String place = "";
+
+        switch (season) {
+            case "Summer":
+                if (budget <= 1000) {
+                    location = "Alaska";
+                    place = "Camp";
+                    price = budget * 0.65;
+                } else if (budget > 1000 && budget <= 3000) {
+                    location = "Alaska";
+                    place = "Hut";
+                    price = budget * 0.8;
+                } else {
+                    location = "Alaska";
+                    place = "Hotel";
+                    price = budget * 0.9;
+                }
+                break;
+            case "Winter":
+                if (budget <= 1000) {
+                    location = "Morocco";
+                    place = "Camp";
+                    price = budget * 0.45;
+                } else if (budget > 1000 && budget <= 3000) {
+                    location = "Morocco";
+                    place = "Hut";
+                    price = budget * 0.6;
+                } else {
+                    location = "Morocco";
+                    place = "Hotel";
+                    price = budget * 0.9;
+                }
+                break;
+        }
+        System.out.printf("%s - %s - %.2f", location, place, price);
     }
 }
