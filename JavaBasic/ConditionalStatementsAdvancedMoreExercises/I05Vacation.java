24a0cf013af77771fdb4594afc53435b800236a0
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-06 06:40:53 +0200

    Update for the ninth lecture.

---
+++ JavaBasic/ConditionalStatementsAdvancedMoreExercises/I05Vacation.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsAdvancedMoreExercises;
+
+import java.util.Scanner;
+
+public class I05Vacation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
