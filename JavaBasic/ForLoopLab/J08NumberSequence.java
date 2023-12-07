aabac6837cf89b7a8cb03184116de4b664fbedb0
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-07 07:26:06 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopLab/J08NumberSequence.java
@@ -0,0 +1,28 @@
+package ForLoopLab;
+
+import java.util.Scanner;
+
+public class J08NumberSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int maxNumber = Integer.MIN_VALUE;
+        int minNumber = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int numbers = Integer.parseInt(scanner.nextLine());
+
+            if (maxNumber < numbers) {
+                maxNumber = numbers;
+            }
+
+            if (minNumber > numbers) {
+                minNumber = numbers;
+            }
+        }
+        System.out.printf("Max number: %d\n", maxNumber);
+        System.out.printf("Min number: %d", minNumber);
+    }
+}
