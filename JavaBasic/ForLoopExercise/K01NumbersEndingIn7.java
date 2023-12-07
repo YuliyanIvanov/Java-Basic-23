8ac2791d8039bf72621735e477b70cffc730993b
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-07 07:55:59 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopExercise/K01NumbersEndingIn7.java
@@ -0,0 +1,9 @@
+package ForLoopExercise;
+
+import java.util.Scanner;
+
+public class K01NumbersEndingIn7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
