49963a58eb5decd98cef47be9d3e8b1ae89b2182
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-07 06:27:34 +0200

    Update for the tenth lecture.

--- JavaBasic/ForLoopLab/J01NumbersFrom1To100.java
+++ JavaBasic/ForLoopLab/J01NumbersFrom1To100.java
@@ -1,9 +1,11 @@
 package ForLoopLab;
 
-import java.util.Scanner;
-
 public class J01NumbersFrom1To100 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+
+        for (int num = 1; num <= 100; num++) {
+            System.out.println(num);
+        }
     }
 }
