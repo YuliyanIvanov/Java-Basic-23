1d7defd3bd6f918562248bfdc33531a1ad2a1585
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-11 07:17:37 +0200

    Update for the tenth lecture.

--- JavaBasic/ForLoopExercise/K02HalfSumElement.java
+++ JavaBasic/ForLoopExercise/K02HalfSumElement.java
@@ -6,6 +6,29 @@ public class K02HalfSumElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int numbers = Integer.parseInt(scanner.nextLine());
 
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 1; i <= numbers; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+
+            sum += num;
+
+            if (num > max) {
+                max = num;
+            }
+        }
+        int sumWithOutTaxNumber = sum - max;
+
+        if (max == sumWithOutTaxNumber) {
+            System.out.println("Yes");
+            System.out.println("Sum = " + max);
+        } else {
+            int diff = Math.abs(max - sumWithOutTaxNumber);
+            System.out.println("No");
+            System.out.println("Diff = " + diff);
+        }
     }
 }
