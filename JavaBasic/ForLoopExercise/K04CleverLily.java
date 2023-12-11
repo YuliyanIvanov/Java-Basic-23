28ade99c3c906b308870f8df72513ccf2a6eb9d9
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-11 07:43:56 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopExercise/K04CleverLily.java
@@ -0,0 +1,9 @@
+package ForLoopExercise;
+
+import java.util.Scanner;
+
+public class K04CleverLily {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
