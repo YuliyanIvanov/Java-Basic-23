2313adff70232c6399b12692c606774082e16530
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-11 07:09:58 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopExercise/K02HalfSumElement.java
@@ -0,0 +1,11 @@
+package ForLoopExercise;
+
+import java.util.Scanner;
+
+public class K02HalfSumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
