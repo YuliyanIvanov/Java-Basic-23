69197922ba65c172846b1bd7b336b5c0107e3d19
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-12 07:04:48 +0200

    Update for the tenth lecture.

--- JavaBasic/ForLoopExercise/K05Salary.java
+++ JavaBasic/ForLoopExercise/K05Salary.java
@@ -6,6 +6,31 @@ public class K05Salary {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int openTabs = Integer.parseInt(scanner.nextLine());
+        int salary = Integer.parseInt(scanner.nextLine());
 
+        for (int i = 1; i <= openTabs; i++) {
+            String site = scanner.nextLine();
+
+            switch (site) {
+                case "Facebook":
+                    salary -= 150;
+                    break;
+                case "Instagram":
+                    salary -= 100;
+                    break;
+                case "Reddit":
+                    salary -= 50;
+                    break;
+            }
+            if (salary <= 0) {
+                break;
+            }
+        }
+        if (salary > 0) {
+            System.out.println(salary);
+        } else {
+            System.out.println("You have lost your salary.");
+        }
     }
 }
