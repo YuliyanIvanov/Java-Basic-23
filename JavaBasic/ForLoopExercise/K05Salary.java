dc996848e0e18b3a83120a42a408f683a27fdb8b
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-12 06:48:14 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopExercise/K05Salary.java
@@ -0,0 +1,11 @@
+package ForLoopExercise;
+
+import java.util.Scanner;
+
+public class K05Salary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
