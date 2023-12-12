1c9b62313d7ed6d67a80abe462156fef2ba6260a
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-12 07:20:04 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopExercise/K07TrekkingMania.java
@@ -0,0 +1,10 @@
+package ForLoopExercise;
+
+import java.util.Scanner;
+
+public class K07TrekkingMania {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
