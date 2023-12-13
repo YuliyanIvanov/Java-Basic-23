d50488864fa8c7284df7f09c7e8c388f22af1ad5
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-13 07:38:11 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopMoreExercises/L02Hospital.java
@@ -0,0 +1,9 @@
+package ForLoopMoreExercises;
+
+import java.util.Scanner;
+
+public class L02Hospital {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
