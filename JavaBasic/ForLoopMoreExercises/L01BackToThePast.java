e27e09529ddd2d790fd0ba958059b4ab9260007e
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-13 07:09:40 +0200

    Update for the tenth lecture.

--- JavaBasic/ForLoopMoreExercises/L01BackToThePast.java
+++ JavaBasic/ForLoopMoreExercises/L01BackToThePast.java
@@ -1,4 +1,11 @@
 package ForLoopMoreExercises;
 
+import java.util.Scanner;
+
 public class L01BackToThePast {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
