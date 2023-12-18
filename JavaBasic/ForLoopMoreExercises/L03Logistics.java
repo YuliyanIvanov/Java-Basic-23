ca2bafe2e0cf480bdbb5dcedde3789dc75b75095
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2023-12-18 06:35:45 +0200

    Update for the tenth lecture.

---
+++ JavaBasic/ForLoopMoreExercises/L03Logistics.java
@@ -0,0 +1,11 @@
+package ForLoopMoreExercises;
+
+import java.util.Scanner;
+
+public class L03Logistics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
