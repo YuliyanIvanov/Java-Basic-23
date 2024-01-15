f600849570739744bd2865a7f284d07bc08d88d7
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-15 07:37:16 +0200

    Update

---
+++ JavaBasic/WhileLoopLab/M03SumNumbers.java
@@ -0,0 +1,18 @@
+package WhileLoopLab;
+
+import java.util.Scanner;
+
+public class M03SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        while (sum < number) {
+            int currentNum = Integer.parseInt(scanner.nextLine());
+            sum += currentNum;
+        }
+        System.out.println(sum);
+    }
+}
