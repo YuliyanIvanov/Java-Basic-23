7c6045714b52dc871148d7973308299279e8bdff
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-15 07:41:39 +0200

    Update

---
+++ JavaBasic/WhileLoopLab/M05AccountBalance.java
@@ -0,0 +1,10 @@
+package WhileLoopLab;
+
+import java.util.Scanner;
+
+public class M05AccountBalance {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
