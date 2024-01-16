94e9a13015413602a52cb2489d4036db28b16733
Yuliyan Ivanov <Julyan.Sv.Ivanov@gmail.com>
2024-01-16 06:40:36 +0200

    Update

--- JavaBasic/WhileLoopLab/M05AccountBalance.java
+++ JavaBasic/WhileLoopLab/M05AccountBalance.java
@@ -6,5 +6,34 @@ public class M05AccountBalance {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+        double sum = 0;
+
+        //четът се входни данни до получаване на командата
+
+        while (!input.equals("NoMoreMoney")) {
+            double myMoney = Double.parseDouble(input);
+
+            //Проверка дали парите са повече от 0
+            //Принтира съобщение ако се въведе грешна сума
+
+            if (myMoney < 0) {
+                System.out.println("Invalid operation");
+                break;
+            }
+
+            //принтира се колко пари са постъпили
+
+            sum += myMoney;
+            System.out.printf("Increase: %.2f\n", myMoney);
+
+            //продължава да чете входни данни при не въведена команда
+
+            input = scanner.nextLine();
+        }
+
+        //принтира се крайният баланс по сметката
+
+        System.out.printf("Total: %.2f", sum);
     }
 }
